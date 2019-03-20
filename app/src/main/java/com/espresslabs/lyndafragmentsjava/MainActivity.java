14462dd0eda2278e2025c068416c3bf445c367aa
Viet <viet@espresslabs.com>
2019-03-20 14:11:59 -0400

    Decide whether to insert a fragment or launch a separate activity based on screen size and orientaion

--- app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
+++ app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
@@ -1,14 +1,20 @@
 package com.espresslabs.lyndafragmentsjava;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean mTablet;
+    private ViewGroup fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +23,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ViewGroup fragmentContainer = findViewById(R.id.detail_fragment_container);
+        fragmentContainer = findViewById(R.id.detail_fragment_container);
         mTablet = (fragmentContainer != null);
 
         TextView tvOut = findViewById(R.id.textOut);
         tvOut.setText("Fragments side-by-side? " + mTablet);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewDetailFragment();
+            }
+        });
+    }
+
+    private void viewDetailFragment() {
+
+        if (mTablet == true) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            DetailActivityFragment fragment = new DetailActivityFragment();
+
+            // create fragment transaction
+            fragmentManager.beginTransaction()
+                    .add(R.id.detail_fragment_container, fragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 }
