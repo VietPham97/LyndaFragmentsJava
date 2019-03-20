c1f927d8cec8c239b3b22caa6bd6b6ad6d93c14e
Viet <viet@espresslabs.com>
2019-03-20 10:26:35 -0400

    Add a fragment with Java code

--- app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
+++ app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
@@ -2,12 +2,24 @@ package com.espresslabs.lyndafragmentsjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SimpleFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment = new SimpleFragment();
+    }
+
+    public void addFragment(View view) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
     }
 }
