d581b823584e2612c261c1e1df5c0332caca3987
Viet <viet@espresslabs.com>
2019-03-20 13:36:19 -0400

    Detect screen with resource selectors to decide either to use multiples activities or place fragments side-by-side

--- app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
+++ app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
@@ -2,40 +2,26 @@ package com.espresslabs.lyndafragmentsjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mOut;
-    private boolean mUseFragment;
+    private boolean mTablet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        mOut = findViewById(R.id.text_out);
-    }
+        ViewGroup fragmentContainer = findViewById(R.id.detail_fragment_container);
+        mTablet = (fragmentContainer != null);
 
-    public void onMeasureBtnClick(View view) {
-        // Create an instance of ScreenUtility
-        ScreenUtility su = new ScreenUtility(this);
-
-        // Display the result in dimensions
-        mOut.setText(String.format("Width: %s, Height: %s", su.getDpWidth(), su.getDpHeight()));
-
-        if (su.getDpWidth() >= 820) {
-            // Time to place fragments side-by-side instead of using 2 different activities
-            mUseFragment = true;
-        } else {
-            mUseFragment = false;
-        }
-
-        Toast.makeText(this,
-                "Using Fragment? " + mUseFragment, Toast.LENGTH_SHORT).show();
-
+        TextView tvOut = findViewById(R.id.textOut);
+        tvOut.setText("Fragments side-by-side? " + mTablet);
     }
 
 }
