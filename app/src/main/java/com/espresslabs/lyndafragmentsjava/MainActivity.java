1425f3d099a8f8a2f67558e23b0200d64fde7994
Viet <viet@espresslabs.com>
2019-03-20 11:15:32 -0400

    Pass arguments to a fragment

--- app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
+++ app/src/main/java/com/espresslabs/lyndafragmentsjava/MainActivity.java
@@ -1,22 +1,25 @@
 package com.espresslabs.lyndafragmentsjava;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SimpleFragment fragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fragment = new SimpleFragment();
     }
 
     public void addFragment(View view) {
+
+        Bundle arguments = new Bundle();
+        arguments.putString(SimpleFragment.MESSAGE_KEY, "Passed as an argument");
+
+        SimpleFragment fragment = new SimpleFragment();
+        fragment.setArguments(arguments);
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
--- app/src/main/java/com/espresslabs/lyndafragmentsjava/SimpleFragment.java
+++ app/src/main/java/com/espresslabs/lyndafragmentsjava/SimpleFragment.java
@@ -1,11 +1,15 @@
 package com.espresslabs.lyndafragmentsjava;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +17,25 @@ import android.view.ViewGroup;
  */
 public class SimpleFragment extends Fragment {
 
+    public static final String MESSAGE_KEY = "message_key";
 
     public SimpleFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_simple, container, false);
 
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            String message = arguments.getString(MESSAGE_KEY);
+            TextView tvMessage = view.findViewById(R.id.message);
+            tvMessage.setText(message);
+        }
+
+        return view;
+    }
 }
